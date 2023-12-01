440c3066b7fa79680e420fe1e9f9f10ad3a0ec39
Niel Rohling <niel_samuel.rohling@mail.schwarz>
2023-12-01 10:02:04 +0100

    Solve Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,50 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
+    check(part2(readInput("Day01_test2")) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+}
+
+
+fun part1(input: List<String>): Int = input.asSequence()
+    .map { line ->
+        buildList {
+            for (ch in line) {
+                ch.digitToIntOrNull()?.let(::add)
+            }
+        }
+    }.map { it.first() * 10 + it.last() }.sum()
+
+fun part2(input: List<String>): Int =
+    input.asSequence().map { line ->
+        buildList {
+            for (i in line.indices) {
+                (line[i].digitToIntOrNull() ?: line.substring(i).wordToDigitOrNull())?.let(::add)
+            }
+        }
+    }.map { it.first() * 10 + it.last() }.sum()
+
+val stringIntMap = mapOf(
+    "one" to 1,
+    "two" to 2,
+    "three" to 3,
+    "four" to 4,
+    "five" to 5,
+    "six" to 6,
+    "seven" to 7,
+    "eight" to 8,
+    "nine" to 9,
+    "zero" to 0
+)
+
+fun String.wordToDigitOrNull(): Int? {
+    for ((word, digit) in stringIntMap) {
+        if (startsWith(word)) {
+            return digit
+        }
+    }
+    return null
 }
